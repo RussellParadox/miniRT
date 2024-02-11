8438505d86ad9476e7633d772b9c71a347c75137
gdornic <gdornic@student.42perpignan.fr>
2024-02-11 11:02:57 +0100

    vector normalized protected

--- src/model/scene/object/vector/normalize.c
+++ src/model/scene/object/vector/normalize.c
@@ -6,7 +6,7 @@
 /*   By: gdornic <gdornic@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 14:59:10 by gdornic           #+#    #+#             */
-/*   Updated: 2024/02/11 08:58:19 by gdornic          ###   ########.fr       */
+/*   Updated: 2024/02/11 10:59:32 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,17 @@ t_vector	vector_normalized(t_vector v)
 	float	norm;
 
 	norm = vector_norm(v);
-	v.x /= norm;
-	v.y /= norm;
-	v.z /= norm;
+	if (norm < 0.0001)
+	{
+		v.x = 0;
+		v.y = 0;
+		v.z = 1;
+	}
+	else
+	{
+		v.x /= norm;
+		v.y /= norm;
+		v.z /= norm;
+	}
 	return (v);
 }
